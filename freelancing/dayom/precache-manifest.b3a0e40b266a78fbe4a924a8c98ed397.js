0d2d854196ef40fb3fc8799ab7136b19e606b6bd
Jordan Balintac <jbalintac@outlook.com>
2021-05-11 18:58:16 +1200

    Migrating from lightsail

---
+++ freelancing/dayom/precache-manifest.b3a0e40b266a78fbe4a924a8c98ed397.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb0abe646cc9bbd3fd0a",
+    "url": "/static/js/app.0d248421.chunk.js"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "ee53a6d2e73c9c7d7830",
+    "url": "/static/js/2.214a4012.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3bdf9aca591716b8a723e25c68c0cf4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cf9bacf97592b9aeeb413d8b035d52",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
